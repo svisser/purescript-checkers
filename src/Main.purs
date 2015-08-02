42c58548c3b913953f2b417885beb40e8194efc5
Simeon Visser <simeon87@gmail.com>
2015-08-02 03:17:16 +0100

    Render piece of both players

--- src/Main.purs
+++ src/Main.purs
@@ -2,22 +2,26 @@ module Main where
 
 import Control.Monad.Eff
 import Data.Array
-import Data.Int
+import Data.Int (even, odd, toNumber)
 import Data.Maybe
 import Graphics.Canvas
+import Math
 import Prelude
 
 type Player = String
 
-type Square = { x :: Int, y :: Int, rx :: Number, ry :: Number, color :: String }
+type Piece = { color :: String }
+
+type Square = { x :: Int, y :: Int, rx :: Number, ry :: Number, color :: String, piece :: Maybe Piece }
 
 type Grid = Array Square
 
-renderSize :: Number
 renderSize = 60.0
+colorOne = "red"
+colorTwo = "white"
 
-createGrid :: Int -> Int -> Grid
-createGrid width height = do
+createGrid :: Int -> Int -> Int -> Grid
+createGrid width height layers = do
   x <- 0 .. (width - 1)
   y <- 0 .. (height - 1)
   let rx = (toNumber x) * renderSize
@@ -25,7 +29,14 @@ createGrid width height = do
       color = if disj (conj (even x) (odd y)) (conj (even y) (odd x))
               then "#d18b47"
               else "#ffce9e"
-  return { x: x, y: y, rx: rx, ry: ry, color: color }
+      hasPlayerOne = conj (y < layers) (disj (conj (even y) (odd x)) (conj (even x) (odd y)))
+      hasPlayerTwo = conj (y >= height - layers) (disj (conj (even y) (odd x)) (conj (even x) (odd y)))
+      piece = if hasPlayerOne
+              then Just { color: colorOne }
+              else if hasPlayerTwo
+              then Just { color: colorTwo }
+              else Nothing
+  return { x: x, y: y, rx: rx, ry: ry, color: color, piece: piece }
 
 renderSquare :: forall e. Context2D -> Unit -> Square -> Eff (canvas :: Canvas | e) Unit
 renderSquare ctx _ square = do
@@ -33,7 +44,18 @@ renderSquare ctx _ square = do
   setFillStyle square.color ctx
   fillRect ctx { x: square.rx, y: square.ry, w: renderSize, h: renderSize }
   restore ctx
-  return unit
+  case square.piece of
+    Just piece -> do
+      save ctx
+      setFillStyle piece.color ctx
+      fillPath ctx $ arc ctx { x: square.rx + 0.5 * renderSize,
+                               y: square.ry + 0.5 * renderSize,
+                               r: (renderSize / 2.0) * 0.8,
+                               start: 0.0,
+                               end: 2.0 * pi }
+      restore ctx
+      return unit
+    _ -> return unit
 
 render :: forall e. Context2D -> Grid -> Eff (canvas :: Canvas | e) Unit
 render ctx grid = do
@@ -48,6 +70,6 @@ main = do
   case element of
     Just canvas -> do
       ctx <- getContext2D canvas
-      render ctx (createGrid 8 8)
+      render ctx (createGrid 8 8 3)
       return unit
     _ -> return unit
